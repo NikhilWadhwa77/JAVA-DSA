edcf1f0280f6021b974d88eaa84ffbd5db02db3a
Nikhil Wadhwa <101242510+NikhilWadhwa77@users.noreply.github.com>
2023-08-02 21:45:38 +0530

    Create String-findCharacterPercentage.java

---
+++ String-findCharacterPercentage.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class CharPercentage {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.next();
+		
+		int charCount = 0, numCount = 0, specialCharCount = 0;
+		
+		for(int i = 0; i < str.length(); i++) {
+			char current = str.charAt(i);
+			
+			if(current >= 65 && current <= 90 || current >= 97 && current <= 122) {
+				charCount++;
+			}
+			else if(current >= 48 && current <= 57) {
+				numCount++;
+			}
+			else {
+				specialCharCount++;
+			}
+		}
+		
+		int totalChars = str.length();
+		
+		double charPercentage = ((double)charCount/(double)totalChars)*100;
+		double numPercentage = ((double)numCount/(double)totalChars)*100;
+		double specialCharPercentage = ((double)specialCharCount/(double)totalChars)*100;
+		
+		System.out.println("Characters :" + Math.round(charPercentage) + "%");
+		System.out.println("Special Characters :" + Math.round(specialCharPercentage) + "%");
+		System.out.println("Numbers :" + Math.round(numPercentage) + "%");
+		
+		sc.close();
+
+	}
+
+}
