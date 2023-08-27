c7d354f50c2593fae910de28e2b8dac86c163729
Nikhil Wadhwa <101242510+NikhilWadhwa77@users.noreply.github.com>
2023-08-27 20:26:45 +0530

    Update String_2114.java

--- Strings/String_2114.java
+++ Strings/String_2114.java
@@ -1,1 +1,15 @@
+class Solution {
+    public int mostWordsFound(String[] sentences) {
 
+        int maxLen = Integer.MIN_VALUE;
+
+        for(int i = 0; i < sentences.length; i++){
+            String[] eachSen = sentences[i].split(" ");
+            if(eachSen.length > maxLen){
+                maxLen = eachSen.length;
+            }
+        }
+
+        return maxLen;
+    }
+}
